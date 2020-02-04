0a998ab67acc049d555dbc406f04ac1fbdbe2080
Sbaah <sbaah12@gmail.com>
2020-02-04 00:12:57 -0500

    creating a static for reaxt

--- server.js
+++ server.js
@@ -5,7 +5,7 @@
 const express = require("express");
 const cors = require("cors");
 const path = require("path");
-const bodyParser = require('body-parser');
+const bodyParser = require("body-parser");
 const app = express();
 const HTTP_PORT = process.env.PORT || 8080;
 // Or use some other port number that you like better
@@ -15,19 +15,19 @@ app.use(bodyParser.json());
 // Add support for CORS
 app.use(cors());
 
-
-
 // ################################################################################
 // Data model and persistent store setup
 
-const manager = require('./manager.js');
-const m = manager("mongodb+srv://A_db1:A_db1@assignment1-oenmq.mongodb.net/db-a1?retryWrites=true&w=majority");
+const manager = require("./manager.js");
+const m = manager(
+  "mongodb+srv://A_db1:A_db1@assignment1-oenmq.mongodb.net/db-a1?retryWrites=true&w=majority"
+);
 
 // ################################################################################
 // Deliver the app's home page to browser clients
 
 app.get("/", (req, res) => {
-    res.sendFile(path.join(__dirname, "/index.html"));
+  res.sendFile(path.join(__dirname, "/index.html"));
 });
 
 // ################################################################################
@@ -35,96 +35,101 @@ app.get("/", (req, res) => {
 
 // Get all
 app.get("/api/vehicles", (req, res) => {
-    // Call the manager method
-    m.vehicleGetAll()
-        .then((data) => {
-            res.json(data);
-        })
-        .catch((error) => {
-            res.status(500).json({
-                "message": error
-            });
-        })
+  // Call the manager method
+  m.vehicleGetAll()
+    .then(data => {
+      res.json(data);
+    })
+    .catch(error => {
+      res.status(500).json({
+        message: error
+      });
+    });
 });
 
 // Get one
 app.get("/api/vehicle/:vehicleId", (req, res) => {
-    // Call the manager method
-    m.vehicleGetById(req.params.vehicleId)
-        .then((data) => {
-            res.json(data);
-        })
-        .catch(() => {
-            res.status(404).json({
-                "message": "Resource not found"
-            });
-        })
+  // Call the manager method
+  m.vehicleGetById(req.params.vehicleId)
+    .then(data => {
+      res.json(data);
+    })
+    .catch(() => {
+      res.status(404).json({
+        message: "Resource not found"
+      });
+    });
 });
 
 // Add new
 app.post("/api/vehicle", (req, res) => {
-    // Call the manager method
-    m.vehicleAdd(req.body)
-        .then((data) => {
-            res.json(data);
-        })
-        .catch((error) => {
-            res.status(500).json({
-                "message": error
-            });
-        })
+  // Call the manager method
+  m.vehicleAdd(req.body)
+    .then(data => {
+      res.json(data);
+    })
+    .catch(error => {
+      res.status(500).json({
+        message: error
+      });
+    });
 });
 
 // Edit existing
 app.put("/api/vehicle/:vehicleId", (req, res) => {
-    // Call the manager method
-    m.vehicleEdit(req.body)
-        .then((data) => {
-            res.json(data);
-        })
-        .catch(() => {
-            res.status(404).json({
-                "message": "Resource not found"
-            });
-        })
+  // Call the manager method
+  m.vehicleEdit(req.body)
+    .then(data => {
+      res.json(data);
+    })
+    .catch(() => {
+      res.status(404).json({
+        message: "Resource not found"
+      });
+    });
 });
 
 // Delete item
 app.delete("/api/vehicle/:vehicleId", (req, res) => {
-    // Call the manager method
-    m.vehicleDelete(req.params.vehicleId)
-        .then(() => {
-            res.status(204).end();
-        })
-        .catch(() => {
-            res.status(404).json({
-                "message": "Resource not found"
-            });
-        })
+  // Call the manager method
+  m.vehicleDelete(req.params.vehicleId)
+    .then(() => {
+      res.status(204).end();
+    })
+    .catch(() => {
+      res.status(404).json({
+        message: "Resource not found"
+      });
+    });
 });
-
-
-
 
 // ################################################################################
 // Resource not found (this should be at the end)
 
 app.use((req, res) => {
-    res.status(404).send("Resource not found");
+  res.status(404).send("Resource not found");
 });
 
-
+// Step 3
+// Serve static assets if in production
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("a1-app/build"));
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "a1-app", "build", "index.html"));
+  });
+}
 
 // ################################################################################
 // Attempt to connect to the database, and
 // tell the app to start listening for requests
 
-m.connect().then(() => {
-        app.listen(HTTP_PORT, () => {
-            console.log("Ready to handle requests on port " + HTTP_PORT)
-        });
-    })
-    .catch((err) => {
-        console.log("Unable to start the server:\n" + err);
-        process.exit();
+m.connect()
+  .then(() => {
+    app.listen(HTTP_PORT, () => {
+      console.log("Ready to handle requests on port " + HTTP_PORT);
     });
+  })
+  .catch(err => {
+    console.log("Unable to start the server:\n" + err);
+    process.exit();
+  });
